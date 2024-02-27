80595e9a2d10389a28219274ccbc6deb675168c7
CP Payne <charlethical1@gmail.com>
2024-02-27 18:40:14 +0200

    Added maze class. Can now generate cells based on input

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from graphics import Window, Line, Point
-from maze import Cell
+from maze import Cell, Maze
 
 def main():
     win = Window(1000, 1000)
 
-    cell1 = Cell(win)
-    cell1.draw(200, 200, 250, 250)
+    # cell1 = Cell(win)
+    # cell1.draw(200, 200, 250, 250)
 
-    cell2 = Cell(win)
-    cell2.draw(300, 300, 350, 350)
+    # cell2 = Cell(win)
+    # cell2.draw(300, 300, 350, 350)
 
-    cell1.draw_move(cell2, undo=True)
+    # cell1.draw_move(cell2, undo=True)
+    maze = Maze(50, 50, 10, 10, 50, 50, win)
 
 
     win.wait_for_close()
--- maze.py
+++ maze.py
@@ -1,4 +1,43 @@
 from graphics import Line, Window, Point
+import time
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win) -> None:
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+        self._create_cells()
+    
+    def _create_cells(self):
+        for row in range(self.num_rows):
+            row_cells = []
+            for col in range(self.num_cols):
+                cell = Cell(self._win)
+                #cell.draw((col*self.cell_size_x)+self.x1, (row*self.cell_size_y)+self.y1,((col+1)*self.cell_size_x)+self.x1, ((row+1)*self.cell_size_y)+self.y1)
+                row_cells.append(cell)
+            self._cells.append(row_cells)
+        for row in range(self.num_cols):
+            for col in range(self.num_rows):
+                self._draw_cell(row, col)
+                
+
+    def _draw_cell(self, col, row):
+        x1 = (col*self.cell_size_x)+self.x1
+        y1 = (row*self.cell_size_y)+self.y1
+        x2 = ((col+1)*self.cell_size_x)+self.x1
+        y2 = ((row+1)*self.cell_size_y)+self.y1
+        self._cells[col][row].draw(x1, y1, x2, y2)
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
 
 class Cell:
     def __init__(self, window) -> None:
