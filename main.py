41af9490a3fa42f0e111ff9cf0020faa4cebf670
CP Payne <charlethical1@gmail.com>
2024-02-28 13:00:49 +0200

    Added an iterative method (using a stack) to generate the maze (break the walls)

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def main():
 
     # Large maze sizes causes program to reach recursion depth to fast. 
     # Not recommended to change the recursion depth though :O
-    sys.setrecursionlimit(10000)
+    #sys.setrecursionlimit(10000)
 
     # x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win, seed=None
-    maze = Maze(50, 50, 30, 30, 30, 30, win)
+    maze = Maze(20, 20, 30, 30, 20, 20, win)
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -20,6 +20,8 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
+        #self._break_walls_iterative()
+        self._reset_cells_visited()
     
     def _create_cells(self):
         for row in range(self.num_rows):
@@ -96,15 +98,44 @@ class Maze:
     # TODO: Recursion limit reached to fast. Use stack to simulate recursion
     def _break_walls_r(self, row, col):
         self._cells[row][col].visited = True
+        #self._animate()
         while True:
             to_visit = self._get_valid_adjacent_cells_i(row, col)
             if len(to_visit) == 0:
                 self._draw_cell(row, col) 
+                self._animate()
                 return
 
             random_direction = random.choice(to_visit)
             self._break_cell_wall(row, col, random_direction)
             self._break_walls_r(random_direction[0], random_direction[1])
+    def _break_walls_iterative(self):
+        stack = [(0, 0)]
+        self._cells[0][0].visited = True
+
+        while stack:
+            row, col = stack.pop()
+            self._animate()  # If you want to animate the process
+            to_visit = self._get_valid_adjacent_cells_i(row, col)
+
+            if to_visit:
+                # Push the current cell back onto the stack
+                stack.append((row, col))
+                # Choose a random adjacent cell to visit next
+                next_row, next_col = random.choice(to_visit)
+                # Break the wall between the current cell and the chosen adjacent cell
+                self._break_cell_wall(row, col, (next_row, next_col))
+                # Mark the chosen cell as visited and add it to the stack
+                self._cells[next_row][next_col].visited = True
+                stack.append((next_row, next_col))
+                # Draw the current and adjacent cells to visually update the maze
+                self._draw_cell(row, col)
+                self._draw_cell(next_row, next_col)
+
+    def _reset_cells_visited(self):
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                self._cells[row][col].visited=False
             
             
             
